831cd7ee6bca4fe6b8792463ee49c1189a957679
Miroljub <miroljub1995>
2020-03-08 15:34:00 +0100

    Added lane detectors

--- main.py
+++ main.py
@@ -39,46 +39,6 @@ import traci  # noqa
 
 stepSize = 0.05
 
-def generate_routefile():
-    random.seed(42)  # make tests reproducible
-    N = 3600  # number of time steps
-    # demand per second from different directions
-    pWE = 1. / 10
-    pEW = 1. / 11
-    pNS = 1. / 30
-    with open("data/cross.rou.xml", "w") as routes:
-        print("""<routes>
-        <vType id="typeWE" accel="0.8" decel="4.5" sigma="0.5" length="5" minGap="2.5" maxSpeed="16.67" \
-guiShape="passenger"/>
-        <vType id="typeNS" accel="0.8" decel="4.5" sigma="0.5" length="7" minGap="3" maxSpeed="25" guiShape="bus"/>
-
-        <route id="right" edges="51o 1i 2o 52i" />
-        <route id="left" edges="52o 2i 1o 51i" />
-        <route id="down" edges="54o 4i 3o 53i" />""", file=routes)
-        vehNr = 0
-        for i in range(N):
-            if random.uniform(0, 1) < pWE:
-                print('    <vehicle id="right_%i" type="typeWE" route="right" depart="%i" />' % (
-                    vehNr, i), file=routes)
-                vehNr += 1
-            if random.uniform(0, 1) < pEW:
-                print('    <vehicle id="left_%i" type="typeWE" route="left" depart="%i" />' % (
-                    vehNr, i), file=routes)
-                vehNr += 1
-            if random.uniform(0, 1) < pNS:
-                print('    <vehicle id="down_%i" type="typeNS" route="down" depart="%i" color="1,0,0"/>' % (
-                    vehNr, i), file=routes)
-                vehNr += 1
-        print("</routes>", file=routes)
-
-# The program looks like this
-#    <tlLogic id="0" type="static" programID="0" offset="0">
-# the locations of the tls are      NESW
-#        <phase duration="31" state="GrGr"/>
-#        <phase duration="6"  state="yryr"/>
-#        <phase duration="31" state="rGrG"/>
-#        <phase duration="6"  state="ryry"/>
-#    </tlLogic>
 def isYellow():
     return traci.trafficlight.getPhase("center") % 2 != 0
 
@@ -90,32 +50,36 @@ def run():
         traci.simulationStep()
         seconds += stepSize
 
-        print("currentPhase: {}, stepsInPhase: {}".format(currentPhase, stepsInPhase))
-        print("Is yellow: {}".format(isYellow()))
-        if isYellow():
-            if round(stepsInPhase, 6) == round((3 // stepSize) * stepSize, 6):
-                currentPhase = random.randrange(3) * 2
-                print("Random: {}".format(currentPhase))
-                traci.trafficlight.setPhase("center", currentPhase)
-                stepsInPhase = 0
-            else:
-                stepsInPhase += stepSize
+        cars = traci.vehicle.getIDList()
+        for car in cars:
+            print("Vehicle: {}, lane: {}".format(car, traci.vehicle.getLaneID(car)))
+
+        # print("currentPhase: {}, stepsInPhase: {}".format(currentPhase, stepsInPhase))
+        # print("Is yellow: {}".format(isYellow()))
+        if isYellow() and stepsInPhase >= 3:
+            currentPhase = random.randrange(4) * 2
+            print("Random: {}".format(currentPhase))
+            traci.trafficlight.setPhase("center", currentPhase)
+            stepsInPhase = 0
+        elif stepsInPhase >= 15:
+            currentPhase += 1
+            traci.trafficlight.setPhase("center", currentPhase)
+            stepsInPhase = 0
         else:
-            if round(stepsInPhase, 6) == round((15 // stepSize) * stepSize, 6):
-                currentPhase += 1
-                traci.trafficlight.setPhase("center", currentPhase)
-                stepsInPhase = 0
-            else:
-                stepsInPhase += stepSize
+            stepsInPhase += stepSize
     traci.close()
     sys.stdout.flush()
 
 
 # this is the main entry point of this script
 if __name__ == "__main__":
+    out = 'out'
+    if not os.path.exists(out):
+        os.mkdir(out)
+        
     sumoBinary = checkBinary('sumo-gui')
     traci.start([sumoBinary, "-c", "cross.sumocfg",
-                             "--tripinfo-output", "tripinfo.xml",
+                             "--tripinfo-output", os.path.join(out, "tripinfo.xml"),
                              "--step-length", "{}".format(stepSize)])
 
     run()
