f206a90d029d0b234ffafe33baa2f35291ca5c71
Miroljub <miroljub1995>
2020-03-09 23:27:41 +0100

    track vehicles

--- main.py
+++ main.py
@@ -44,39 +44,66 @@ def isYellow():
 
 def run():
     seconds = 0
-    currentPhase = 0
-    stepsInPhase = 0
+    current_phase = 0
+    steps_in_phase = 0
+
+    detectors = {}
+    for det in traci.lanearea.getIDList():
+        detectors[det] = {'cars': {}}
+    num_passed_light = 0
+
+    print(detectors)
+
     while traci.simulation.getMinExpectedNumber() > 0:
         traci.simulationStep()
         seconds += stepSize
 
-        cars = traci.vehicle.getIDList()
-        for car in cars:
-            print("Vehicle: {}, lane: {}".format(car, traci.vehicle.getLaneID(car)))
+        # cars = traci.vehicle.getIDList()
+        # for car in cars:
+        #     print("Vehicle: {}, lane: {}".format(car, traci.vehicle.getLaneID(car)))
 
         # print("currentPhase: {}, stepsInPhase: {}".format(currentPhase, stepsInPhase))
         # print("Is yellow: {}".format(isYellow()))
-        if isYellow() and stepsInPhase >= 3:
-            currentPhase = random.randrange(4) * 2
-            print("Random: {}".format(currentPhase))
-            traci.trafficlight.setPhase("center", currentPhase)
-            stepsInPhase = 0
-        elif stepsInPhase >= 15:
-            currentPhase += 1
-            traci.trafficlight.setPhase("center", currentPhase)
-            stepsInPhase = 0
+        # print(traci.lanearea.getLastStepVehicleNumber('e2Detector_r_left_0'))
+        traci.lanearea.getLastStepVehicleIDs('e2Detector_r_left_0')
+
+        for det in detectors:
+            cars = traci.lanearea.getLastStepVehicleIDs(det)
+            for car in cars:
+                if car not in detectors[det]['cars']:
+                    detectors[det]['cars'][car] = car
+                    print("New car {} in detector {}".format(car, det))
+
+            for car in list(detectors[det]['cars']):
+                if car not in cars:
+                    del detectors[det]['cars'][car]
+                    num_passed_light += 1
+                    print("Car {} left detector {}".format(car, det))
+
+
+        if isYellow() and steps_in_phase >= 3:
+            current_phase = random.randrange(4) * 2
+            print("Random: {}".format(current_phase))
+            traci.trafficlight.setPhase("center", current_phase)
+            steps_in_phase = 0
+        elif steps_in_phase >= 15:
+            current_phase += 1
+            traci.trafficlight.setPhase("center", current_phase)
+            steps_in_phase = 0
         else:
-            stepsInPhase += stepSize
+            steps_in_phase += stepSize
+    print("Num of cars: {}".format(num_passed_light))
+    print("Time: {}".format(traci.simulation.getTime()))
+    print(num_passed_light * 60 / traci.simulation.getTime())
     traci.close()
     sys.stdout.flush()
-
 
 # this is the main entry point of this script
 if __name__ == "__main__":
     out = 'out'
     if not os.path.exists(out):
         os.mkdir(out)
-        
+
     sumoBinary = checkBinary('sumo-gui')
     traci.start([sumoBinary, "-c", "cross.sumocfg",
                              "--tripinfo-output", os.path.join(out, "tripinfo.xml"),
