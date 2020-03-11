397603fab31f6bc42813ddfef4566d1e297448e5
Miroljub <miroljub1995>
2020-03-11 18:02:31 +0100

    added classes for detectors

---
+++ detectors.py
@@ -0,0 +1,41 @@
+import traci
+
+
+class Detectors:
+    def __init__(self):
+        self.detectors = []
+        for det in traci.lanearea.getIDList():
+            self.detectors.append(Detector(det))
+
+        self.num_passed = 0
+        self.num_cars = 0
+
+    def update(self):
+        self.num_passed = 0
+        self.num_cars = 0
+        for det in self.detectors:
+            det.update()
+            self.num_passed += det.num_passed
+            self.num_cars += det.num_cars()
+
+class Detector:
+    def __init__(self, id_string):
+        self.id = id_string
+        self.num_arrived = 0
+        self.num_passed = 0
+
+        self.cars = []
+
+    def update(self):
+        cars = traci.lanearea.getLastStepVehicleIDs(self.id)
+        for car in cars:
+            if car not in self.cars:
+                self.num_arrived += 1
+                self.cars.append(car)
+        for car in list(self.cars):
+            if car not in cars:
+                self.cars.remove(car)
+                self.num_passed += 1
+
+    def num_cars(self):
+        return len(self.cars)
--- main_q_learning.py
+++ main_q_learning.py
@@ -7,6 +7,7 @@ from __future__ import division
 import os
 import sys
 import random
+import time
 
 # we need to import python modules from the $SUMO_HOME/tools directory
 if 'SUMO_HOME' in os.environ:
@@ -18,25 +19,68 @@ else:
 from sumolib import checkBinary  # noqa
 import traci  # noqa
 import numpy as np
+from detectors import Detectors
 
-stepSize = 0.05
+STEP_SIZE = 0.05
+SIMULATION_DURATION = 400000
+CHUNK_DURATION = 180
 
 def isYellow():
     return traci.trafficlight.getPhase("center") % 2 != 0
 
-def combine_substates(s1, s2, n):
-    i, j = s1, s2
+def intervals_to_substate(i1, i2, n):
+    i, j = i1, i2
     if i > j:
         i, j = j, i
     return int((-(i ** 2) + i * (2 * n - 1) + 2 * j) / 2)
 
-def num_cars_to_substate(n):
+def num_cars_to_intervals(n):
     if n > 4:
         return 2
     elif n > 0:
         return 1
     else:
         return 0
+
+def map_lambda(arr, func):
+    return np.array([func(x) for x in arr])
+
+def get_next_action(q, s):
+    k = 2
+    row = q[s]
+    mapped_row = map_lambda(row, lambda x: k ** x)
+    sum_of_elements = np.sum(mapped_row)
+    probs = map_lambda(mapped_row, lambda x: x / sum_of_elements)
+    selected = np.random.choice(q.shape[1], p=probs)
+    return selected
+
+def det_pair_to_substate(det_pair, num_intervals):
+    d1, d2 = det_pair
+    num_cars_d1 = len(traci.lanearea.getLastStepVehicleIDs(d1))
+    num_cars_d2 = len(traci.lanearea.getLastStepVehicleIDs(d2))
+
+    interval1 = num_cars_to_intervals(num_cars_d1)
+    interval2 = num_cars_to_intervals(num_cars_d2)
+
+    return intervals_to_substate(interval1, interval2, num_intervals)
+
+def substates_to_state(substates, num_substates):
+    result = 0
+    k = 1
+    for ss in reversed(substates):
+        result += k * ss
+        k *= num_substates
+    return result
+
+def get_current_state():
+    det_pairs = [
+        ('e2Detector_r_up_0', 'e2Detector_r_down_0'),
+        ('e2Detector_r_up_1', 'e2Detector_r_down_1'),
+        ('e2Detector_r_right_0', 'e2Detector_r_left_0'),
+        ('e2Detector_r_right_1', 'e2Detector_r_left_1')]
+
+    substates = np.array([det_pair_to_substate(pair, 3) for pair in det_pairs])
+    return substates_to_state(substates, 6)
 
 def run():
     seconds = 0
@@ -45,54 +89,38 @@ def run():
     #         v0  v1  v2
     # stanja: 0, 1-4, 5+  (v0 x v0), (v0 x v1), (v0 x v2), (v1 x v1), (v1 x v2), (v2 x v2)
     #                         s0         s1         s2         s3         s4         s5
-    states_per_det = 3
+    states_per_det = 6
     num_of_states = states_per_det ** 4
 
     num_of_actions = 4
-    matrix = np.random.random_sample((num_of_states, num_of_actions))
-    print("Matrix: shape: {}, \n{}".format(matrix.shape, matrix))
+    q = np.random.random_sample((num_of_states, num_of_actions))
+    print("Matrix: shape: {}, \n{}".format(q.shape, q))
+    get_next_action(q, 0)
 
-    detectors = {}
-    for det in traci.lanearea.getIDList():
-        detectors[det] = {'cars': {}}
-    det_pairs = [
-        ('e2Detector_r_up_0', 'e2Detector_r_down_0'),
-        ('e2Detector_r_up_1', 'e2Detector_r_down_1'),
-        ('e2Detector_r_right_0', 'e2Detector_r_left_0'),
-        ('e2Detector_r_right_1', 'e2Detector_r_left_1')]
-
+    detectors = Detectors()
     
-    num_passed_light = 0
-    print(detectors)
-
     prev_eval = sys.float_info.max
 
-    while traci.simulation.getMinExpectedNumber() > 0:
+    flow = 0
+    seconds_in_chunk = 0
+
+    while seconds < SIMULATION_DURATION:
         traci.simulationStep()
-        seconds += stepSize
+        detectors.update()
+        seconds += STEP_SIZE
 
-        for det in detectors:
-            cars = traci.lanearea.getLastStepVehicleIDs(det)
-            for car in cars:
-                if car not in detectors[det]['cars']:
-                    detectors[det]['cars'][car] = car
-                    # print("New car {} in detector {}".format(car, det))
-
-            for car in list(detectors[det]['cars']):
-                if car not in cars:
-                    del detectors[det]['cars'][car]
-                    num_passed_light += 1
-                    # print("Car {} left detector {}".format(car, det))
+        current_state = get_current_state()
+        print("State: {}".format(current_state))
 
         if isYellow() and steps_in_phase >= 3:
-            current_phase = random.randrange(4) * 2
+            # current_phase = random.randrange(4) * 2
+            current_phase = get_next_action(q, current_state) * 2
             print("Random: {}".format(current_phase))
             traci.trafficlight.setPhase("center", current_phase)
             steps_in_phase = 0
             
-            sum_of_cars = 0
-            for det in detectors:
-                sum_of_cars += len(traci.lanearea.getLastStepVehicleIDs(det))
+            sum_of_cars = detectors.num_cars
+            num_passed_light = detectors.num_passed
 
             curr_eval = 0
             if sum_of_cars != 0:
@@ -104,14 +132,26 @@ def run():
             reward = curr_eval - prev_eval
             print("Reward: {} - {} = {}".format(curr_eval, prev_eval, reward))
             prev_eval = curr_eval
-
             num_passed_light = 0
+            if seconds_in_chunk >= CHUNK_DURATION:
+                # save chunk
+                pass
+
         elif steps_in_phase >= 15:
             current_phase += 1
             traci.trafficlight.setPhase("center", current_phase)
             steps_in_phase = 0
         else:
-            steps_in_phase += stepSize
+            steps_in_phase += STEP_SIZE
+
+        
+
+        if seconds_in_chunk >= CHUNK_DURATION:
+            seconds_in_chunk = 0
+        else:
+            seconds_in_chunk += STEP_SIZE
+
+    num_passed_light = detectors.num_passed
     print("Num of cars: {}".format(num_passed_light))
     print("Time: {}".format(traci.simulation.getTime()))
     print(num_passed_light * 60 / traci.simulation.getTime())
@@ -127,6 +167,6 @@ if __name__ == "__main__":
     sumoBinary = checkBinary('sumo-gui')
     traci.start([sumoBinary, "-c", "cross.sumocfg",
                              "--tripinfo-output", os.path.join(out, "tripinfo.xml"),
-                             "--step-length", "{}".format(stepSize)])
+                             "--step-length", "{}".format(STEP_SIZE)])
 
     run()
